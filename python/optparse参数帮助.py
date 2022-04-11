84df2a4773cd6dac86dc1ae365aa6c05d4a9b93b
xiaopengfei <xiaopengfei@bestwo.com>
2022-04-11 16:27:48 +0800

    tools

---
+++ python/optparse参数帮助.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# @Author: fly
+# @Date:   2022-04-11 16:26:15
+# @Last Modified by:   fly
+# @Last Modified time: 2022-04-11 16:27:14
+
+import optparse
+
+parser = optparse.OptionParser()
+
+# dest 存储字段，type 指定参数类型, metavar 提醒参数大写
+parser.add_option("-n", "--number", dest="number", help='test args', default='12', type=int, metavar="NUM")
+
+parser.add_option("-f", "--file", dest="filename",
+                  help="write report to FILE", metavar="FILE")
+
+# store_false 解析到 -q，会被赋予 False 值。 对应 store_true
+parser.add_option("-q", "--quiet",
+                  action="store_false", dest="verbose", default=True,
+                  help="don't print status messages to stdout")
+
+(options, args) = parser.parse_args()
+
+# python test.py
+print(options.verbose)  # True
